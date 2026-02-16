21f03eb1bb3986b8cedc4d12b8e4af51bc7e0e86
Ahiisivi <arttu.hiisivirta@student.hamk.fi>
2026-02-16 11:50:48 +0200

    Initial commit for hosted version of Phonebook

---
+++ index.js
@@ -0,0 +1,115 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+const cors = require('cors')
+
+morgan.token('body', (req) => {
+  return JSON.stringify(req.body)
+})
+
+app.use(express.json())
+// app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+app.use(cors())
+
+let persons = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello from puhelinluettelo backend</h1>')
+})
+
+app.get('/api/persons/', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+    const personsCount = persons.length
+    const timeStamp = new Date()
+    response.send(`<p>The phonebook has info about ${personsCount} persons</p>
+                    <p>The date is: ${timeStamp}</p>`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.use(express.static('dist'))
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+const generateId = () => {
+  return String(Math.floor(Math.random() * 10000))
+}
+
+app.post('/api/persons/', (request, response) => {
+  const body = request.body
+
+  if (!body.name) {
+    return response.status(400).json({
+      error: 'Missing name'
+    })
+  }
+
+  if (!body.number) {
+    return response.status(400).json({
+      error: 'Missing number'
+    })
+  }
+
+  if (persons.find(p => p.name === body.name)) {
+      return response.status(400).json({ 
+        error: 'Name already exists' 
+      })
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number
+  }
+
+  persons = persons.concat(person)
+  console.log(person)
+  response.json(person)
+})
+
+
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
